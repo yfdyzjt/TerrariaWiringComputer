29a4b3082b7ce27731ec755fd1df0219ddebb06f
yfdyzjt <yfdyzjt@gmail.com>
2024-03-06 13:31:27 +0800

    Fix Lua script bug

--- wiring/data_memory/data_rom_256KB.lua
+++ wiring/data_memory/data_rom_256KB.lua
@@ -21,7 +21,7 @@ function TryReadUInt32()
 			bytes[i]=bin.ReadByte()
 		end
 	end
-	return bytes[0]~(bytes[1]<<8)~(bytes[2]<<16)~(bytes[3]<<24)
+	return ((bytes[0]&0xFF)<<0)|((bytes[1]&0xFF)<<8)|((bytes[2]&0xFF)<<16)|((bytes[3]&0xFF)<<24)
 end
 
 function IsZero(data,bit)

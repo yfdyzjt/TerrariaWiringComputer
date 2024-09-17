f41a2cdafb564e522e6b4aebee73063e3a8e6d61
yfdyzjt <yfdyzjt@gmail.com>
2024-09-17 13:04:04 +0800

    Add Startup Script

--- startup.bat
+++ startup.bat
@@ -1,1 +1,3 @@
+@echo off
 docker run -it --rm -v %CD%:/TerrariaWiringComputer yfdyzjt/terraria-wiring-computer
+pause
--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker run -it --rm -v ${PWD}:/TerrariaWiringComputer yfdyzjt/terraria-wiring-computer

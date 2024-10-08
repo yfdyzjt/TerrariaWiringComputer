5593beba01d1e75f5ab13c90bb9bf1335a0f0ebb
yfdyzjt <yfdyzjt@gmail.com>
2024-10-08 22:12:26 +0800

    Add Hardware Data Rom 8KB And 16KB

---
+++ hardware/wiring/memory/data/data_rom_16KB.lua
@@ -0,0 +1,11 @@
+AddrPos = {{2, 4}, {3, 6}, {6, 4}, {5, 6}, {7, 4}, {8, 6}, {11, 4}, {10, 6}}
+
+local MaxLine : int = 256
+local MaxRow : int = 2
+
+local OffsetX : int = 54
+local OffsetY : int = 21
+
+function Write(sch, bin)
+	return Include(self, "./hardware/wiring/**/data_rom.lua").Write(sch, bin, MaxLine, MaxRow, OffsetX, OffsetY)
+end
